ace7a70288cf95b42f91af7103dbd16e1eb3e52e
wyshee14 <weiyee1401@gmail.com>
2025-10-19 23:42:38 +0800

    cpp01: added ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,36 @@
+# include <iostream>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";       // Variable declaration
+
+    // Prints the memory address of the string variable
+    std::cout << "Memory address of str: " << &str <<std::endl;
+    std::cout << "Value of str: " << str << std::endl;
+
+    // Pointer(*) store the memory address as its value
+    // & operator refers to the memory address of a variable
+    std::string* stringPTR = &str;      // Pointer decalaration
+    std::cout << "Memory address held by stringPTR: " << std::endl;
+    // Dereference: Output the value of str with pointer(*) 
+    // By accessing the memory address of str
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    // Prints the memory address that stores the memory address of str (different value)
+    // std::cout << "Memory address of stringPTR: " << &stringPTR << std::endl;
+    // Change the value of the pointer 
+    // *stringPTR = "Value is changed";
+    // std::cout << *stringPTR << std::endl;
+    // std::cout << str << std::endl;
+
+    // A reference variable is an alias (another name) for an exisitng variable
+    // Both str and stringREF refer to the same memory location and same value
+    std::string &stringREF = str;       //Reference declaration
+    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+    // This will change the value of str too
+    // stringREF = "I AM NOT BRAIN";
+    std::cout << "Value pointed to by stringREF: " << stringREF <<std::endl;
+}
+
+// & - Reference operator
+// * - Dereference operator (Not declaration)
+// When used in declaration, is a pointer variable
