7121b60dc500c07977820d2aa7cc3b9a2dc1b4f1
wyshee14 <weiyee1401@gmail.com>
2025-10-26 01:03:17 +0800

    cpp01:ex05: added test to verify the output and funtion called
    Changes:
    -added sstream and cassert
    -assert the output to console with the expected
    -stores console output to a string stream buffer
    -added '\n' at the end of expected output

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -1,4 +1,6 @@
 # include "Harl.hpp"
+# include <sstream>     //use ostringstream, write into string instead of console
+# include <cassert>     //provide assert() macro
 
 int main()
 {
@@ -17,7 +19,30 @@ int main()
     (instance.*f)("debug");
     // Dereference the instance to call the member fucntion
     (instancep->*f)("info");
+
+    //TODO: add test for each level
+    std::cout << "Tesing with assetions: " << std::endl;
+    std::ostringstream out; // stores output in memory instead printing out in console
+    std::streambuf* old_cout = std::cout.rdbuf(out.rdbuf()); // Redirect cout to write into out
     
+    // Expected output
+    const std::string expectedDebug = "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+    const std::string expectedInfo = "I cannot believe adding extra bacon costs more money. You didn't putenough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+    const std::string expectedWarning = "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n";
+    const std::string expectedError = "This is unacceptable! I want to speak to the manager now.\n";
+    
+    instance.complain("debug");
+    // std::cout.rdbuf(old_cout);
+    // std::cout << "Expected: [" << expectedDebug << "]\n";
+    // std::cout << "out: " << out.str();
+    // assertion failed if return 0
+    assert(out.str() == expectedDebug); // out.str()Get captured output as a string
+    out.str(""); //Clear buffer
+    // std::cout.rdbuf(out.rdbuf());
+
+    // restore cout before redirection
+    std::cout.rdbuf(old_cout);
+    std::cout << "All tests passed" << std::endl;
     return 0;
 }
 
